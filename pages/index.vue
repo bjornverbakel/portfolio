75b107410db6db1e9584d0b9f5c3de5969e1d8a7
Bjorn Verbakel <bjorn.verbakel@student.fontys.nl>
2025-07-19 13:53:13 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="backdrop"></div>
+  <img src="/logo.svg" alt="Logo" class="mb-4" />
+  <nav>
+    <ul>
+      <li><p>About</p></li>
+      <li><p>Projects</p></li>
+      <li><p>Contact</p></li>
+    </ul>
+  </nav>
+</template>
