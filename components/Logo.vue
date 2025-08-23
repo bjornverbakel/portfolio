284cf3155842a9216e75213935b4b8760fec2391
Bjorn Verbakel <bjorn.verbakel@student.fontys.nl>
2025-08-23 16:53:27 +0200

    fix build test

--- components/Logo.vue
+++ components/Logo.vue
@@ -29,7 +29,7 @@ function handleLogoClick() {
 
 <template>
   <div id="logo" class="flex m-8 z-10" ref="logoRef">
-    <img 
+    <NuxtImg 
       draggable="false" 
       src="/icons/logo.svg" 
       alt="Logo" 
