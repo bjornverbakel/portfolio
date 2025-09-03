dc75c20cdf79a2587b2c1867656c75313239751a
bjorn <bjornverbak2006@gmail.com>
2025-09-03 10:17:41 +0200

    added social links

--- components/content/Contact.vue
+++ components/content/Contact.vue
@@ -1,37 +1,45 @@
 <script setup lang="ts">
 import { NuxtImg } from "#components";
+import { Icon } from '@iconify/vue'
 </script>
 
 <template>
   <article class="w-full flex flex-col gap-16 text-justify">
     <section class="flex flex-col gap-4">
       <h1 class="text-xl" data-fade-color>Contact</h1>
-      <p data-fade-color>You can contact me at...</p>
-      <div>
-        
+      <p data-fade-color>You can contact me at:</p>
+      <div class="flex flex-col gap-4" data-fade-opacity>
+        <a class="btn mix-blend-difference flex align-middle gap-2 underline underline-offset-4 decoration-2"
+          href="mailto:#">
+          <Icon icon="material-symbols:mail-sharp" height="none" :style="{ width: '2rem', height: '2rem' }" />
+          <span class="mix-blend-difference">email@email.com</span>
+        </a>
+        <a class="btn mix-blend-difference flex align-middle gap-2 underline underline-offset-4 decoration-2"
+          href="tel:#">
+          <Icon icon="material-symbols:call-sharp" height="none" :style="{ width: '2rem', height: '2rem' }" />
+          <span class="mix-blend-difference">1234567890</span>
+        </a>
       </div>
     </section>
     <section class="flex flex-col gap-4">
       <h2 class="text-lg" data-fade-color>You can also find me on:</h2>
       <div class="flex gap-4" data-fade-opacity>
-        <a
-          href="https://www.linkedin.com/in/bjorn-verbakel-651938349/"
-          class="social-btn btn"
-          ><NuxtImg src="/images/linkedin.svg" alt="LinkedIn" /><span
-            >LinkedIn</span
-          ></a
-        >
-        <a href="https://github.com/bjornverbakel" class="social-btn btn"
-          ><NuxtImg src="/images/github.svg" alt="Github" /><span
-            >Github</span
-          ></a
-        >
+        <a href="https://www.linkedin.com/in/bjorn-verbakel-651938349/" class="social-btn btn">
+          <NuxtImg src="/images/linkedin.svg" alt="LinkedIn" /><span>LinkedIn</span>
+        </a>
+        <a href="https://github.com/bjornverbakel" class="social-btn btn">
+          <NuxtImg src="/images/github.svg" alt="Github" /><span>Github</span>
+        </a>
       </div>
     </section>
   </article>
 </template>
 
 <style scoped>
+a {
+  cursor: none;
+}
+
 .social-btn {
   display: flex;
   align-items: center;
@@ -67,6 +75,7 @@ import { NuxtImg } from "#components";
   width: 100%;
   height: 100%;
 }
+
 .social-btn::after {
   width: 0;
   height: 4px;
@@ -75,6 +84,7 @@ import { NuxtImg } from "#components";
 .social-btn:hover::before {
   width: 0;
 }
+
 .social-btn:hover::after {
   width: 100%;
 }
@@ -82,6 +92,7 @@ import { NuxtImg } from "#components";
 .social-btn.active::before {
   width: 0;
 }
+
 .social-btn.active::after {
   width: 100%;
 }
